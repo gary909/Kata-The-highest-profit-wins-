7ec01e3f6a4f1d46974071f9f679c7c1a6c45bfa
gary909 <garywhite61@hotmail.com>
2022-11-04 13:00:28 +0100

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function minMax(arr) {
+    return [0, 0]; // fix me!
+}
+
+console.log(minMax([1, 2, 3, 4, 5])); // [1, 5]
+console.log(minMax([2334454, 5])); // [5, 2334454]
+console.log(minMax([5])); // [5, 5]
